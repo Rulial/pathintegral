f9c0347d3ed1637a1b2d0633416cff097c67f32a
Rulial <>
2022-09-08 11:46:41 +0000

    Update

--- src/components/svgs/swumr.tsx
+++ src/components/svgs/swumr.tsx
@@ -7,7 +7,7 @@ function Swumr(props: any): JSX.Element {
       width="30"
       height="45"
       version="1"
-      viewBox="10 10 80 80"
+      viewBox="30 10 105 60"
     >
       <path
         d="M480 1398c0-7 263-781 352-1035l92-263h43c24 0 43 1 43 3 0 1-74 214-164 472-90 259-191 549-223 645l-59 175-42 3c-23 2-42 2-42 0z"
