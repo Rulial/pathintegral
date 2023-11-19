f84ae86c1f762f3800d438bb6772295d9acca0ab
Rulial <98750732+Rulial@users.noreply.github.com>
2023-11-19 13:53:22 +0000

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Index() {
         
       <img
           src="/mg01.png"
+          link="https://pathintegral0.notion.site/Ventures-f538ee0d88a44ed8ab2130cf692084bd?pvs=4"
           height="640"
           width="640"
           alt="A?I"
