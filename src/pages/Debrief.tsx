4e0833812705bcddf5d680f2b02fa96437b3f70e
Rulial <98750732+Rulial@users.noreply.github.com>
2024-06-22 10:55:48 +0000

    Your commit message here

--- src/pages/Debrief.tsx
+++ src/pages/Debrief.tsx
@@ -15,12 +15,11 @@ export default function Index() {
     
     <Header />
       
-      
 
         <div className={sharedStyles.layout}>
           <a href="https://pathintegral0.notion.site/Ventures-f538ee0d88a44ed8ab2130cf692084bd?pvs=4">
         <img
-            src="/Debrief3.png"
+            src="/Debrief4.png"
             height="640"
             width="640"
             alt="A?I"
