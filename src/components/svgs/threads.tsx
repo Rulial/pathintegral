692b81f847798af1bce7e6bbe816c6aa6bb46f97
Rohit Bindal <98750732+Rulial@users.noreply.github.com>
2025-07-05 06:04:38 -0400

    Update threads.tsx

--- src/components/svgs/threads.tsx
+++ src/components/svgs/threads.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React from 'react';
 
-const thread = (props) => (
+// Your component code here
+
+const Thread = (props: any) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="16"
