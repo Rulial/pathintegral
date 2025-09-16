3b4a1915f1ad846dd53ab4b57cd97c973bff2bad
Leonard Pahlavi/ Rohit Bindal <98750732+Rulial@users.noreply.github.com>
2025-09-16 12:13:32 +0000

    this means nothing else

--- src/pages/agency.tsx
+++ src/pages/agency.tsx
@@ -1,26 +1,28 @@
-//intelligence agency as in leverage
+//line numbers mean nothing else and not commnication of any kind at all.
 import sharedStyles from '../styles/shared.module.css';
 import React from 'react';
 import PathIntegralHeader from '../components/mainPage';
 // The 'Link' and 'React' imports were removed because they were unused.
-
-
-
 
 export default function Agency() {
   return (
     <>
 
 
+
 <PathIntegralHeader />
       
-      <div className={sharedStyles.layout}>
-        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">ANNA + SOLO FAQ</a>
+      <div className={sharedStyles.layout}> 
+        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">ANNA + SOLO AI NATIVE FAQ</a> 
         <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Communication FAQ</a>
         <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Personal FAQ</a>
         <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Current Affairs FAQ</a>
-        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Insights</a>
-     
+        
+        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Tweets</a>
+        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Words</a>
+        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">The Grand Loop</a>
+        <a href="https://drive.google.com/drive/folders/1iBQqsuHpR0VhquoeDZ74FYjZ5FXsBKxE">Prince of Persia</a>
+
       </div>
 
     </>
